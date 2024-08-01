3adc9adbbf026bc83f64805a4d3ef8931a7059d9
MayankDDOS <mayankrecovery561@gmail.com>
2024-08-01 21:24:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7233448536:AAF5JecaeJIISNmSNM1Wndeuj8avdE5bH-Q")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Mayank_0369")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1905264343")
 
 
 
