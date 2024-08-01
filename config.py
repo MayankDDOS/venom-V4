3023cbea4b03c46d8cbd044df07bb915154ba4b3
MayankDDOS <mayankrecovery561@gmail.com>
2024-08-01 21:26:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7233448536:AAF5JecaeJIISNmSNM1Wndeuj8avdE5bH-Q")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mayank_0369")
+OWNER_USERNAME=("Mayank_0369")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1905264343")
